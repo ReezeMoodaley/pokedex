aa41c0d510e531d6b476d35d2bb297879880e9d1
ReezeMoodaley <reezemoodaley@gmail.com>
2025-11-03 17:03:08 +0200

    Working pokemon retrieval

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import PokeMain from "@/components/pokeMain";
 
 /* To do list:
 
@@ -9,10 +8,56 @@ import PokeMain from "@/components/pokeMain";
 
 */
 
-export default function Home() {
+type PokemonQueryRes = {
+  name: string;
+  url: string;
+};
+
+type PokemonCardData = {
+  name: string;
+  id: string;
+  sprite: string;
+};
+
+async function getAllPokemon(): Promise<PokemonCardData[]> {
+  try {
+    /* Initial fetching of the pokemon */
+    const pokeURL = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
+    const res = await fetch(pokeURL);
+
+    const pokeData: { results: PokemonQueryRes[] } = await res.json();
+    const pokeObjects = pokeData.results;
+
+    /* Setup of the array of promises */
+    const pokePromises = pokeObjects.map((p) => {
+      return fetch(p.url).then((res) => res.json());
+    });
+
+    /* Execute all the promises concurrently */
+    const pokeDetails = await Promise.all(pokePromises);
+
+    /* PokeDetails data Transform */
+    const finalPokeData: PokemonCardData[] = pokeDetails.map(
+      (detail, index) => ({
+        name: pokeDetails[index].name,
+        id: pokeDetails[index].id,
+        sprite: detail.sprites.front_default,
+      })
+    );
+    console.log(finalPokeData);
+    return finalPokeData;
+  } catch (e) {
+    console.error("Failed to fetch Pokemon data", e);
+    throw e;
+  }
+}
+
+export default async function Home() {
+  const pokemonData = await getAllPokemon();
+
   return (
     <div>
-      <PokeMain />
+      <h2>Pokemon List</h2>
     </div>
   );
 }
---
+++ components/PokeCard.tsx
@@ -0,0 +1,6 @@
+function PokeCard() {
+  /* This will serve as the base card class that will be used for displaying each pokemon*/
+
+  return <div>PokeCard</div>;
+}
+export default PokeCard;
